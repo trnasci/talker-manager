8d9b8a7fbcbc2f6bc1e13504c08a9a220a09c983
Thiago Reis Nascimento <trnasci@gmail.com>
2022-12-14 20:43:18 -0300

    resolução do requisito 02

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -12,4 +12,14 @@ talkerRoute.get('/talker', async (req, res) => {
     return res.status(200).json(talkers);
 });
 
+talkerRoute.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = await readFile(filePath);
+    const talkersFind = talkers.filter((e) => e.id === Number(id));
+    if (talkersFind.length === 0) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });        
+    }
+    return res.status(200).json(talkersFind[0]);
+});
+
 module.exports = talkerRoute;
