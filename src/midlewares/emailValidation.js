f383f5f86fd86053faf891f55ba342de8767775a
Thiago Reis Nascimento <trnasci@gmail.com>
2022-12-15 10:34:25 -0300

    resolução do requisito 04

---
+++ src/midlewares/emailValidation.js
@@ -0,0 +1,15 @@
+const emailValidation = (req, res, next) => {
+    const { email } = req.body;
+    function validateEmail(em) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(em);
+      }
+    if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    if (!validateEmail(email)) {
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    } 
+    
+    next();
+};
+
+module.exports = emailValidation;
--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,16 +1,12 @@
 const express = require('express');
+const emailValidation = require('../midlewares/emailValidation');
+const passwordValidation = require('../midlewares/passwordValidation');
 const { generateToken } = require('../utils/generateToken');
 
 const loginRoute = express.Router();
 loginRoute.use(express.json());
 
-loginRoute.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const loginPerson = {
-        email,
-        password,
-    };
-    console.log(loginPerson);
+loginRoute.post('/login', emailValidation, passwordValidation, (req, res) => {
     const token = generateToken();      
     return res.status(200).json({ token });
   });
