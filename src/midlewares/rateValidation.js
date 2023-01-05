41b954bda63f69a1d5e541164ec926285050c331
Thiago Reis Nascimento <trnasci@gmail.com>
2023-01-05 16:05:02 -0300

    Resolução requisito 06

--- src/midlewares/rateValidation.js
+++ src/midlewares/rateValidation.js
@@ -1,7 +1,13 @@
+const validRate = (rate) => {
+  if (!rate && rate !== 0) {
+    return true;
+  }
+};
+
 function rateValidation(req, res, next) {
-    const { talk } = req.body;
-    if (!talk.rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
-    if (Number(talk.rate) < 1 || Number(talk.rate) > 5 || !Number.isInteger(Number(talk.rate))) {
+    const { talk: { rate } } = req.body;
+    if (validRate(rate)) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+    if ((Number(rate) < 1 || Number(rate) > 5) || !Number.isInteger(Number(rate))) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
     }
     
